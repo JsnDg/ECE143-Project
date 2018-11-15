02de234a98b1604b7dea848091dd05fcc04ab652
johnnyzhang123 <33502662+johnnyzhang123@users.noreply.github.com>
2018-11-14 23:04:29 -0800

    Update ISI.py
    Fixed some small details based on feedback during meeting today and added Linear Regression to analyze ISI

--- ISI.py
+++ ISI.py
@@ -4,9 +4,12 @@ Analysis of ISI with respect to FFMC and Wind. Factors affecting FFMC are also i
 '''
 import numpy as np
 import matplotlib.pyplot as plt
+import sklearn
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
 from numpy import genfromtxt
+
 fire_data=genfromtxt('forestfires.csv',delimiter=',')
-print(fire_data.shape)
 
 FFMC=fire_data[1:517,4]
 area=fire_data[1:517,12]
@@ -18,40 +21,47 @@ rh=fire_data[1:517,8]
 temp=fire_data[1:517,9]
 fire=[]
 fire_final=[]
+print(fire_data.shape)
+
 for i in range(0,516):
     if ISI[i]>50:
-        print(i)
-        fire_final=np.delete(fire_data,i)
+        print('Outlier in line:', i+2)
+        fire_final=np.delete(fire_data,i,0)
     if area[i]>0:
         fire.append('r')
     else:
         fire.append('g')
+print(fire_final.shape)
 
 plt.scatter(ISI,FFMC,color=fire)
-plt.xlabel('ISI')
-plt.ylabel('FFMC')
+plt.xlabel('FFMC')
+plt.ylabel('ISI')
 plt.show()
 
-
-
 plt.scatter(ISI,wind,color=fire)
-plt.xlabel('ISI')
-plt.ylabel('Wind')
+plt.xlabel('Wind')
+plt.ylabel('ISI')
 plt.show()
 
 plt.scatter(ISI,rain,color=fire)
-plt.xlabel('ISI')
-plt.ylabel('Rain')
+plt.xlabel('Rain')
+plt.ylabel('ISI')
 plt.show()
 
 plt.scatter(ISI,rh,color=fire)
-plt.xlabel('ISI')
-plt.ylabel('Relative Humidity')
+plt.xlabel('Relative Humidity')
+plt.ylabel('ISI')
 plt.show()
 
 plt.scatter(ISI,temp,color=fire)
-plt.xlabel('ISI')
-plt.ylabel('Temperature')
+plt.xlabel('Temperature')
+plt.ylabel('ISI')
 plt.show()
 
-print(type(fire_final))
+ISI_X=np.column_stack(([fire[:,4]],[fire[:,10]]))
+ISI_Y=np.column_stack([fire[:,7]])
+reg = linear_model.LinearRegression()
+reg.fit(ISI_X,ISI_Y)
+ISI_Y_pred = reg.predict(ISI_X)
+plt.scatter(ISI_X, ISI_Y,color='black')
+plt.plot(ISI_X, ISI_Y_pred, color='blue', linewidth=3)
