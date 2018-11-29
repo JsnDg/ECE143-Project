fd0c5351f772852210ee2189642a45278230dcfb
johnnyzhang123 <33502662+johnnyzhang123@users.noreply.github.com>
2018-11-29 14:28:51 -0800

    Add files via upload
    Added docstring and comments

--- ISI.py
+++ ISI.py
@@ -2,66 +2,79 @@
 Jianing Zhang 11/12/2018
 Analysis of ISI with respect to FFMC and Wind. Factors affecting FFMC are also included to see if there is any correlation
 '''
+#import necessary modules
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 from numpy import genfromtxt
-
+#read as numpy array
 fire_data=genfromtxt('forestfires.csv',delimiter=',')
-
+#Dissect data into columns, below are primary and direct factors
 FFMC=fire_data[1:517,4]
 area=fire_data[1:517,12]
 wind=fire_data[1:517,10]
 ISI=fire_data[1:517,7]
-
+#Dissect secondary data
 rain=fire_data[1:517,11]
 rh=fire_data[1:517,8]
 temp=fire_data[1:517,9]
-fire=[]
-fire_final=[]
-print(fire_data.shape)
+fire=[]#create array to record fire and non-fire
+
 
 for i in range(0,516):
     if ISI[i]>50:
-        print('Outlier in line:', i+2)
-        fire_final=np.delete(fire_data,i,0)
+        print('Outlier in line:', i+2)#Obvious outlier picked through visualization. The data is marked and will be removed to train machine learning model.
     if area[i]>0:
-        fire.append('r')
+        fire.append('r')#Mark fire as red
     else:
-        fire.append('g')
-print(fire_final.shape)
+        fire.append('g')#Mark no fire as green
 
+#Plot ISI and FFMC using scatterplot, a direct factor
 plt.scatter(ISI,FFMC,color=fire)
 plt.xlabel('FFMC')
 plt.ylabel('ISI')
 plt.show()
-
+#Plot ISI and wind,a direct factor
 plt.scatter(ISI,wind,color=fire)
 plt.xlabel('Wind')
 plt.ylabel('ISI')
 plt.show()
-
+#Plot ISI and rain, an indirect factor
 plt.scatter(ISI,rain,color=fire)
 plt.xlabel('Rain')
 plt.ylabel('ISI')
 plt.show()
-
+#Plot ISI and relative humidity(RH), an indirect factor
 plt.scatter(ISI,rh,color=fire)
 plt.xlabel('Relative Humidity')
 plt.ylabel('ISI')
 plt.show()
-
+#Plot ISI and temperature, an indirect factor
 plt.scatter(ISI,temp,color=fire)
 plt.xlabel('Temperature')
 plt.ylabel('ISI')
 plt.show()
-
-ISI_X=np.column_stack(([fire[:,4]],[fire[:,10]]))
-ISI_Y=np.column_stack([fire[:,7]])
+#Investigate further into relationship between ISI and direct factors.
+#Use linear regression model to see whether we can express ISI in terms of FFMC linearly.
+ISI_x1=np.column_stack([fire_data[1:,4]])#Pick out FFMC column from datasheet
+ISI_Y=np.column_stack([fire_data[1:,7]])#Pick out ISI column from datasheet
 reg = linear_model.LinearRegression()
-reg.fit(ISI_X,ISI_Y)
-ISI_Y_pred = reg.predict(ISI_X)
-plt.scatter(ISI_X, ISI_Y,color='black')
-plt.plot(ISI_X, ISI_Y_pred, color='blue', linewidth=3)
+reg.fit(ISI_Y,ISI_x1)#Use linear regression model provided by sklearn
+ISI_Y_pred = reg.predict(ISI_x1)#Try to predict ISI given FFMC values and the linear equation we just found
+plt.scatter(ISI_Y, ISI_x1,color='black')
+plt.plot(ISI_x1, ISI_Y_pred, color='blue', linewidth=3)
+plt.show()#plot the equation and data
+#Linear model has limitations, could be applied only at the clustered areas.
+
+#Use linear regression model to see whether we can express ISI in terms of FFMC linearly.
+ISI_x2=np.column_stack([fire_data[1:,10]])#Pick out wind column from datasheet
+ISI_Y=np.column_stack([fire_data[1:,7]])#Pick out ISI column from datasheet
+reg = linear_model.LinearRegression()
+reg.fit(ISI_Y,ISI_x2)#Use linear regression model provided by sklearn
+ISI_Y_pred = reg.predict(ISI_x2)#Try to predict ISI given wind values and the linear equation we just found
+plt.scatter(ISI_Y, ISI_x2,color='black')
+plt.plot(ISI_x2, ISI_Y_pred, color='blue', linewidth=3)
+plt.show()#plot the equation and data
+#Linear model could not satisfy our demands due to data being too scattered.
