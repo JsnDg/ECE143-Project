b3cf29aa4942455ed0081a57d2c69368c3497b02
Gitikameher <42259008+Gitikameher@users.noreply.github.com>
2018-12-02 14:50:41 -0800

    Update 3d_hist.py
    Added a few comments

--- 3d_hist.py
+++ 3d_hist.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Nov 29 14:13:51 2018
-
-@author: meher
+For plotting the the frequency of fires in different grids of the park
+The areas with fire are filtered first and their frequency of recording a fire is plotted as a 3D plot. 
+Outliers are not included by default as only data with fires is considered.
+area column indicates the spread of the fire. Considered as intensity for all purposes.
 """
-from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
-
+from mpl_toolkits.mplot3d import Axes3D 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 fire=pd.read_csv('forestfires.csv')
+
 intensity=fire.area
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
@@ -17,23 +18,20 @@ X=fire.X
 Y=fire.Y
 x=X.iloc[intensity.nonzero()[0]]
 y=Y.iloc[intensity.nonzero()[0]]
-
-hist, xedges, yedges = np.histogram2d(x, y, bins=4, range=[[0, 9], [0,9]])
-
-# Construct arrays for the anchor positions of the 16 bars.
+#chose fewer bins to make it look less crowded.
+hist, xedges, yedges = np.histogram2d(x,y,bins=4,range=[[0, 9], [0,9]])
 xpos, ypos = np.meshgrid(xedges[:-1] + 0.25, yedges[:-1] + 0.25, indexing="ij")
 xpos = xpos.ravel()
 ypos = ypos.ravel()
 zpos = 0
 
-# Construct arrays with the dimensions for the 16 bars.
 dx = dy = 0.5 * np.ones_like(zpos)
 dz = hist.ravel()
-#plt.ylim(9,0)
-plt.xlim(9,0)
+plt.ylim(9,0)
+#plt.xlim(9,0)
 ax.bar3d(xpos, ypos, zpos, dx, dy, dz, color='b', zsort='average')
-plt.xlabel("x")
-plt.ylabel("y")
-plt.zlabel("frequency of fires")
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.set_zlabel("frequency of fires")
 
 plt.show()
