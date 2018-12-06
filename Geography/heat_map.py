34e699a4de76acbf768958ebfa2b42bcb3b3600d
Gitikameher <42259008+Gitikameher@users.noreply.github.com>
2018-12-05 17:10:40 -0800

    Update heat_map.py
    added some constraints on x and y co-ordinates

--- Geography/heat_map.py
+++ Geography/heat_map.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Nov 18 07:24:23 2018
-
-@author: meher
-"""
-
 from numpy import genfromtxt
 my_data = genfromtxt('forestfires.csv', delimiter=',')
 import numpy as np
@@ -12,23 +6,36 @@ from numpy import array
 import matplotlib.pyplot as plt
 from scipy.interpolate import griddata
 
-Z_dat=my_data[1:517,12]
-X_dat=my_data[1:517,0]
-Y_dat=my_data[1:517,1]
+intensity=my_data[1:517,12]
+X=my_data[1:517,0]
+Y=my_data[1:517,1]
 
-X, Y, Z, = np.array([]), np.array([]), np.array([])
-for i in range(len(X_dat)):
+def plot_heat_map(Z_dat,X_dat,Y_dat):
+    '''
+    For plotting the intensity of fires in different grids identified
+    by the x and y co-ordinates of the grid.
+    '''
+    import numpy as np
+    assert isinstance(Z_dat, np.ndarray)
+    assert isinstance(Y_dat, np.ndarray)
+    assert isinstance(X_dat, np.ndarray)
+    assert 0<X_dat.all()<10
+    assert 0<Y_dat.all()<10
+    assert 0<=Y_dat.all()<1000
+    X, Y, Z, = np.array([]), np.array([]), np.array([])
+    for i in range(len(X_dat)):
         X = np.append(X,X_dat[i])
         Y = np.append(Y,Y_dat[i])
         Z = np.append(Z,Z_dat[i])
-zmin = 1
-zmax = 9
-zi[(zi<zmin) | (zi>zmax)] = 0
-# create x-y points to be used in heatmap
-xi = np.linspace(X.min(),X.max(),1000)
-yi = np.linspace(Y.min(),Y.max(),1000)
-zi = griddata((X, Y), Z, (xi[None,:], yi[:,None]), method='cubic')
-CS = plt.contourf(xi, yi, zi, 15, cmap=plt.cm.gist_heat)
-plt.colorbar()  
-plt.show()
+        #Grids are from 1-9 in both the axes.
+    zmin = 1
+    zmax = 9
+    # create x-y points to be used in heatmap
+    xi = np.linspace(X.min(),X.max(),1000)
+    yi = np.linspace(Y.min(),Y.max(),1000)
+    zi = griddata((X, Y), Z, (xi[None,:], yi[:,None]), method='cubic')
+    CS = plt.contourf(xi, yi, zi, 15, cmap=plt.cm.gist_heat)
+    plt.colorbar()  
+    plt.show()
 
+plot_heat_map(intensity, X, Y)
