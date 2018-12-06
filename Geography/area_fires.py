2ddc06fa177d6d3ae22e6df8821d5e27a7f4d3cf
Gitikameher <42259008+Gitikameher@users.noreply.github.com>
2018-12-05 17:19:26 -0800

    Update area_fires.py

--- Geography/area_fires.py
+++ Geography/area_fires.py
@@ -1,24 +1,29 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Nov 29 21:18:33 2018
-
-@author: meher
-"""
 import pandas as pd
 import matplotlib.pyplot as plt
+fire=pd.read_csv('forestfires.csv')
 intensity=fire.area
-inten=intensity.iloc[intensity.nonzero()[0]]
-total=inten.count()
-small=inten[inten < 50 ].count() 
-large=inten[inten > 50 ].count() 
+
+def plot_areas_with_fires(intensity):
+    '''
+    Plots the large and small fires as a pie chart showing the 
+    probability of occurrence of each
+    '''
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    assert isinstance(intensity, pd.core.series.Series)
+    inten=intensity.iloc[intensity.nonzero()[0]]
+    total=inten.count()
+    small=inten[inten < 50 ].count() 
+    large=inten[inten > 50 ].count() 
 # Pie chart, where the slices will be ordered and plotted counter-clockwise:
-labels = 'small fires','large fires'
-sizes = [small, large]
-explode = (0, 0.1)  # only "explode" the 2nd slice (i.e. 'Hogs')
+    labels = 'small fires','large fires'
+    sizes = [small, large]
+    explode = (0, 0.1)  # only "explode" the 2nd slice 
 
-fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
         shadow=True, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-plt.show()
+    ax1.axis('equal')  
+    plt.show()
+    
+plot_areas_with_fires(intensity)
